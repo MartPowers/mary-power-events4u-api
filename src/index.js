153b0b3e84bf195650ab70ef53e90e8f1066d865
Mary Power <maryy.power@gmail.com>
2024-05-29 11:58:54 -0700

    create events json data and assign images

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const exampleRoute = require('./routes/example');
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Example route
+app.use('/api', exampleRoute);
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
