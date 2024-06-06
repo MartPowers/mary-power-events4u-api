68a0b2d4ea37790db14e53e2d874ed14cbfd4022
Mary Power <maryy.power@gmail.com>
2024-06-06 16:19:44 -0700

    added data and route

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const exampleRoute = require('./routes/example');
+const eventsRoute = require('./routes/events');
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-// Example route
-app.use('/api', exampleRoute);
+// route
+app.use('/events', eventsRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
